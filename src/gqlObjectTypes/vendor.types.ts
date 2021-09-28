62a5543eda8058fd457d23521e20e92aefed47ae
System Administrator <kader.chalil@gmail.com>
2021-09-28 20:03:51 +0400

    Fix

--- src/gqlObjectTypes/vendor.types.ts
+++ src/gqlObjectTypes/vendor.types.ts
@@ -20,8 +20,8 @@ export class AddVendorInput {
     @Field()
     tradelicense: string;
   
-    @Field()
-    category: string;
+    @Field({nullable: true})
+    category?: string;
   
     @Field({nullable: true})
     location: LocationInput;
